0ebf22625d46a24c3a41535f2aa4f7239a43e0fe
sjLim91 <tjdwns9808@gmail.com>
2026-09-13 20:30:03 +0900

    core: track character life stage and birth time

--- Source/LifeLensCore/include/lifelens/Character.h
+++ Source/LifeLensCore/include/lifelens/Character.h
@@ -9,6 +9,7 @@
 #include "Belief.h"
 #include "Genetics.h"
 #include "LifeHistory.h"
+#include "LifeStage.h"
 namespace lifelens {
 struct Character {
     CharacterId id=0;
@@ -22,7 +23,9 @@ struct Character {
     std::vector<CharacterId> parentIds;
     std::vector<CharacterId> childrenIds;
     std::vector<LifeHistoryEntry> lifeHistory;
+    bool hasBirthMinute=false;
     int birthMinute=0;
+    LifeStage lifeStage=LifeStage::Adult;
     double metabolism=1.0;
     double sleepTendency=1.0;
 };
