5e4b4d7309f0e77e45bc2dbc4dd6ced917174160
sjLim91 <tjdwns9808@gmail.com>
2026-09-13 20:30:53 +0900

    core: preserve base physiology across growth stages

--- Source/LifeLensCore/include/lifelens/Character.h
+++ Source/LifeLensCore/include/lifelens/Character.h
@@ -26,6 +26,8 @@ struct Character {
     bool hasBirthMinute=false;
     int birthMinute=0;
     LifeStage lifeStage=LifeStage::Adult;
+    double baseMetabolism=1.0;
+    double baseSleepTendency=1.0;
     double metabolism=1.0;
     double sleepTendency=1.0;
 };
