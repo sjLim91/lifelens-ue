1fc1c657816b2ef9aff40f4ae22ba32e7bdbb386
sjLim91 <tjdwns9808@gmail.com>
2026-09-14 13:53:59 +0900

    world: implement activity anchor reservation lifecycle

--- Source/LifeLens/World/LLActivityAnchor.cpp
+++ Source/LifeLens/World/LLActivityAnchor.cpp
@@ -12,3 +12,65 @@ FVector ALLActivityAnchor::GetUseLocation() const
 {
     return GetActorTransform().TransformPosition(LocalUseOffset);
 }
+
+FTransform ALLActivityAnchor::GetUseTransform() const
+{
+    const FTransform ActorTransform = GetActorTransform();
+    const FVector WorldLocation = ActorTransform.TransformPosition(LocalUseOffset);
+    const FQuat WorldRotation = ActorTransform.TransformRotation(LocalUseRotation.Quaternion());
+    return FTransform(WorldRotation, WorldLocation, FVector::OneVector);
+}
+
+bool ALLActivityAnchor::CanBeUsedBy(FGuid ResidentId) const
+{
+    if (!bEnabled || !ResidentId.IsValid())
+    {
+        return false;
+    }
+
+    return State == ELLActivityAnchorState::Available || ClaimedResidentId == ResidentId;
+}
+
+bool ALLActivityAnchor::IsClaimedBy(FGuid ResidentId) const
+{
+    return ResidentId.IsValid()
+        && ClaimedResidentId == ResidentId
+        && State != ELLActivityAnchorState::Available;
+}
+
+bool ALLActivityAnchor::TryReserve(FGuid ResidentId)
+{
+    if (!CanBeUsedBy(ResidentId))
+    {
+        return false;
+    }
+
+    ClaimedResidentId = ResidentId;
+    if (State == ELLActivityAnchorState::Available)
+    {
+        State = ELLActivityAnchorState::Reserved;
+    }
+    return true;
+}
+
+bool ALLActivityAnchor::MarkInUse(FGuid ResidentId)
+{
+    if (!IsClaimedBy(ResidentId))
+    {
+        return false;
+    }
+
+    State = ELLActivityAnchorState::InUse;
+    return true;
+}
+
+void ALLActivityAnchor::Release(FGuid ResidentId)
+{
+    if (!IsClaimedBy(ResidentId))
+    {
+        return;
+    }
+
+    ClaimedResidentId.Invalidate();
+    State = ELLActivityAnchorState::Available;
+}
