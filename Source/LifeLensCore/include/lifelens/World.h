7f6af3a424054062e310f834dcb24add07ba4458
sjLim91 <tjdwns9808@gmail.com>
2026-09-14 14:29:23 +0900

    core: add civilization resources and storage to world

--- Source/LifeLensCore/include/lifelens/World.h
+++ Source/LifeLensCore/include/lifelens/World.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "Character.h"
 #include "SmartObject.h"
+#include "Civilization.h"
 namespace lifelens {
 struct World {
     int minute=7*60;
@@ -11,6 +12,8 @@ struct World {
     std::mt19937_64 rng{1};
     std::vector<Character> characters;
     std::vector<SmartObject> objects;
+    std::vector<ResourceNode> resourceNodes;
+    std::vector<StorageSite> storageSites;
     explicit World(std::uint64_t s=1) : seed(s?s:1), rng(seed) {}
 };
 }
