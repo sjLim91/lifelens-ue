06fd92af83ecc9b13bac2994f01c457ccff09193
sjLim91 <tjdwns9808@gmail.com>
2026-09-18 20:01:10 +0900

    Fix PlayerCameraManager include path

--- Source/LifeLens/UI/LLObserverMobilePolish.cpp
+++ Source/LifeLens/UI/LLObserverMobilePolish.cpp
@@ -6,7 +6,7 @@
 #include "Framework/Application/SlateApplication.h"
 #include "GenericPlatform/GenericApplication.h"
 #include "GameFramework/PlayerController.h"
-#include "GameFramework/PlayerCameraManager.h"
+#include "Camera/PlayerCameraManager.h"
 
 namespace
 {
