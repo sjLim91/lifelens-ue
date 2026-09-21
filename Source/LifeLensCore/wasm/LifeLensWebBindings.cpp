54a4f9d73545a0adf484a749f14e95b0bef6eb46
sjLim91 <tjdwns9808@gmail.com>
2026-09-21 17:05:17 +0900

    Web v1: add Emscripten bindings

---
+++ Source/LifeLensCore/wasm/LifeLensWebBindings.cpp
@@ -0,0 +1,17 @@
+#include <emscripten/bind.h>
+
+#include "lifelens/WebClientBridge.h"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(lifelens_web_core)
+{
+    class_<lifelens::WebClientBridge>("LifeLensWebClient")
+        .constructor<>()
+        .function("newGame", &lifelens::WebClientBridge::newGame)
+        .function("hasSimulation", &lifelens::WebClientBridge::hasSimulation)
+        .function("runMinutes", &lifelens::WebClientBridge::runMinutes)
+        .function("worldOverviewJson", &lifelens::WebClientBridge::worldOverviewJson)
+        .function("residentsJson", &lifelens::WebClientBridge::residentsJson)
+        .function("terrainWindowJson", &lifelens::WebClientBridge::terrainWindowJson);
+}
